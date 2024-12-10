4f75d7e9f7530787d87cb9ec5be5381eaf8b2e41
tovy14 <virgilt2005@yahoo.com>
2024-12-10 11:20:56 +0200

    created player sprite

--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-
-
-black = (0, 0, 0)
+from player import Player
 
 
 def main():
@@ -14,12 +12,19 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(black)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
